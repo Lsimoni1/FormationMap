8478f2052467b0a5573df41a8f1321d330575d7f
Lsimoni1 <luke.simoni2001@gmail.com>
2026-06-16 16:18:41 -0700

    added functionality for formations to be added to/pulled from the database. Formations are now existing functionally and exchanged between the database and application

--- src/components/CanvasHeader.tsx
+++ src/components/CanvasHeader.tsx
@@ -1,6 +1,8 @@
-import { ArrowUpLeft, ChevronLeft, ChevronRight, Plus, AlignJustify} from 'lucide-react'
+import { ArrowUpLeft, ChevronLeft, ChevronRight, Plus, AlignJustify, Save} from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { useNavigate } from 'react-router-dom'
+import type { Formation } from './FormationMenu';
+
 
 interface CanvasHeaderProps {
     projectTitle: string
@@ -8,62 +10,81 @@ interface CanvasHeaderProps {
     onNext: () => void
     onSave: () => void
     onMenuToggle: () => void
+    currentFormationIndex: number | null
+    formations: Formation[]
+    unsavedChanges: boolean
 }
 
-const CanvasHeader = ({projectTitle, onPrev, onNext, onSave, onMenuToggle}: CanvasHeaderProps) => {
+const CanvasHeader = ({projectTitle, onPrev, onNext, onSave, onMenuToggle, currentFormationIndex, formations, unsavedChanges}: CanvasHeaderProps) => {
     const navigate = useNavigate();
+    let contextLabel = ""
+
+
+    if(currentFormationIndex !== null && formations.length > 0) {
+        contextLabel = "Editing Formation " + (formations[currentFormationIndex].order_index + 1)
+    } else {
+        contextLabel = "Editing New Formation"
+    }
+
+    if(unsavedChanges) {
+        contextLabel += " - Unsaved Changes"
+    }
 
     return(
         <div className="flex items-center justify-between px-4 py-2 border-b-2 border-gray-300"> {/* container for header */}
 
             <div className="flex gap-4 "> {/* container for back button and title*/}
                 <Button /* back button */
-                title="Return to Project Dashboard"
-                onClick={() => navigate('/dashboard')}
-                variant="outline"
-                size="icon-lg"
+                    title="Return to Project Dashboard"
+                    onClick={() => navigate('/dashboard')}
+                    variant="outline"
+                    size="icon-lg"
                 >
                     <ArrowUpLeft />
                 </Button>
 
-                <h1 className="scroll-m-20 text-3xl font-bold tracking-tight">
-                    {projectTitle}
-                </h1>
+                <div className="flex flex-col">
+                    <h1 className="scroll-m-20 text-3xl font-bold tracking-tight">
+                        {projectTitle}
+                    </h1>
+                    <span className="text-sm text-muted-foreground">{contextLabel}</span>
+                </div>
+                
             </div>
 
             <div className="flex gap-6"> {/* container for prev, next, save, and formation menu toggle buttons */}
                 <Button /* previous formation button */
-                title="Switch to previous formation"
-                onClick={onPrev}
-                variant="outline"
-                size="icon-lg"
+                    title="Switch to previous formation"
+                    onClick={onPrev}
+                    variant={currentFormationIndex === 0 || currentFormationIndex === null ? "ghost" : "outline"}
+                    size="icon-lg"
                 >
                     <ChevronLeft />
                 </Button>
 
                 <Button /* next formation button */
-                title="Switch to next formation"
-                onClick={onNext}
-                variant="outline"
-                size="icon-lg"
+                    title="Switch to next formation"
+                    onClick={onNext}
+                    variant={currentFormationIndex === formations.length - 1 || currentFormationIndex === null ? "ghost" : "outline"}
+                    size="icon-lg"
                 >
                     <ChevronRight />
                 </Button>
 
                 <Button /* save new formation button */
-                title="Save canvas state as next formation"
-                onClick={onSave}
-                variant="outline"
-                size="icon-lg"
+                    title={currentFormationIndex === null ? "Save canvas state as new formation" : "Update current formation with canvas state"}
+                    onClick={onSave}
+                    variant="outline"
+                    size="icon-lg"
                 >
-                    <Plus />
+                    {currentFormationIndex === null ? <Plus /> : <Save />}
                 </Button>
 
                 <Button /* toggle formations menu button */
-                title="Toggle Formations Menu"
-                onClick={onMenuToggle}
-                variant="outline"
-                size="icon-lg"
+                    title="Toggle Formations Menu"
+                    onClick={onMenuToggle}
+                    variant="outline"
+                    size="icon-lg"
                 >
                     <AlignJustify />
                 </Button>
--- src/pages/Canvas.tsx
+++ src/pages/Canvas.tsx
@@ -7,21 +7,26 @@ import CanvasHeader from "@/components/CanvasHeader";
 import { supabase } from "@/lib/supabase";
 import { useState, useEffect } from "react";
 import type { Formation } from "@/components/FormationMenu";
+import type { DancerProps } from "@/components/Dancer";
 
-const MOCK_FORMATIONS: Formation[] = [
-  { id: "1", project_id: "test", order_index: 0, dancers: [] },
-  { id: "2", project_id: "test", order_index: 1, dancers: [] },
-  { id: "3", project_id: "test", order_index: 2, dancers: [] },
-  { id: "4", project_id: "test", order_index: 3, dancers: [] },
-  { id: "5", project_id: "test", order_index: 4, dancers: [] },
-  { id: "6", project_id: "test", order_index: 5, dancers: [] },
-  { id: "7", project_id: "test", order_index: 6, dancers: [] },
-]
+// const MOCK_FORMATIONS: Formation[] = [
+//   { id: "1", project_id: "test", order_index: 0, dancers: [] },
+//   { id: "2", project_id: "test", order_index: 1, dancers: [] },
+//   { id: "3", project_id: "test", order_index: 2, dancers: [] },
+//   { id: "4", project_id: "test", order_index: 3, dancers: [] },
+//   { id: "5", project_id: "test", order_index: 4, dancers: [] },
+//   { id: "6", project_id: "test", order_index: 5, dancers: [] },
+//   { id: "7", project_id: "test", order_index: 6, dancers: [] },
+// ]
 
 const Canvas = () => {
   const { projectId } = useParams<{projectId: string}>()
   const [projectName, setProjectName] = useState("")
   const [menuActive, setMenuActive] = useState(false)
+  const [dancers, setDancers] = useState<DancerProps[]>([])
+  const [formations, setFormations] = useState<Formation[]>([])
+  const [currentFormationIndex, setCurrentFormationIndex] = useState<number|null>(null)
+  const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false)
 
   useEffect(() => {
     const fetchProjectName = async () => {
@@ -37,23 +42,99 @@ const Canvas = () => {
     }
     fetchProjectName()
   }, [])
-  
+
+  useEffect(() => {
+    const fetchFormations = async () => {
+      const { data, error } = await supabase
+        .from("formations")
+        .select()
+        .eq("project_id", projectId)
+      if(error != null) {
+        return
+      } else {
+        setFormations(data)
+      }
+    }
+    fetchFormations()
+  }, [])
+
+  //some functions to determine whether the current formation has unsaved changes
+  useEffect(() => {
+    if(dancers.length > 0) {
+      setHasUnsavedChanges(true)
+    }
+  }, [dancers])
+
+  useEffect(() => {
+    setHasUnsavedChanges(false)
+  }, [currentFormationIndex])
+
   //some function to pull all formations belonging to a project,
   //that can be passed to FormationMenu and CanvasHeader 
+  const handleSave = async () => {
+
+    if(currentFormationIndex !== null) {
+      const{ data, error} = await supabase
+        .from("formations")
+        .update({dancers: dancers})
+        .eq("id", formations[currentFormationIndex].id)
+      if(error) {
+        console.error("Error updating formation:", error);
+      } else {
+        console.log("Formation updated successfully!")
+        setFormations(prevFormations  => prevFormations.map(
+          (prevFormation) => prevFormation.id === formations[currentFormationIndex].id
+        ? {...prevFormation, dancers: dancers} :
+        prevFormation))
+        setHasUnsavedChanges(false)
+      }
+    } else {
+      const { data, error } = await supabase
+      .from("formations")
+      .insert({
+        project_id: projectId!,
+        order_index: formations.length,
+        dancers: dancers
+      })
+      .select()
+
+      if(error) {
+        console.error("Error saving formation:", error);
+      } else {
+        console.log("Formation saved successfully!");
+        setFormations([...formations, data[0]])
+        setHasUnsavedChanges(false)
+      }
+    }
+  }
 
   return (
     <ToolProvider>
       <div className="flex flex-col h-screen">
         <CanvasHeader 
-        projectTitle={projectName} 
-        onPrev={() => {}}
-        onNext={() => {}}
-        onSave={() => {}}
-        onMenuToggle={() => setMenuActive(!menuActive)}
+          projectTitle={projectName} 
+          onPrev={() => setCurrentFormationIndex(currentFormationIndex === null ? null : Math.max(0, currentFormationIndex - 1))}
+          onNext={() => setCurrentFormationIndex(currentFormationIndex === null ? 0 : Math.min(formations.length - 1, currentFormationIndex + 1))}
+          onSave={handleSave}
+          onMenuToggle={() => setMenuActive(!menuActive)}
+          currentFormationIndex={currentFormationIndex}
+          formations={formations}
+          unsavedChanges={hasUnsavedChanges}
         />
         <div className="relative flex-1 overflow-hidden">
-          <StageDiagram projectId={projectId!}/>
-          <FormationMenu isViewable={menuActive} formations={MOCK_FORMATIONS} />
+          <StageDiagram 
+            projectId={projectId!} 
+            dancers={dancers} 
+            setDancers={setDancers} 
+            currentFormationIndex={currentFormationIndex}
+            formations={formations} 
+          />
+          <FormationMenu 
+            isViewable={menuActive} 
+            formations={formations} 
+            currentFormationIndex={currentFormationIndex}
+            setCurrentFormationIndex={setCurrentFormationIndex}
+          />
         </div>
         <FloatingToolbar />
       </div>
